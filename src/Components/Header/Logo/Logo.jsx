4310c909a27234c738359865def07820a2366acd
Alexander Salvador <alexander_mya@outlook.com>
2025-08-22 12:36:25 -0500

    Chore: optimizing project

--- src/Components/Header/Logo/Logo.jsx
+++ src/Components/Header/Logo/Logo.jsx
@@ -5,12 +5,17 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className="nav-logo-container">
+    <div className="nav__logo-container">
       <Link to="/" className="link-no-style">
-        <h1 className="nav-logo-title">
-          <SiCoinmarketcap className="nav-logo-icon" />
-          Vitterys
-        </h1>
+        <div className="nav__logo-grid">
+          <div className="nav__logo-container-icon">
+            <SiCoinmarketcap className="nav__logo-icon" />
+          </div>
+          <div className="nav__logo-container-titles">
+            <h1 className="nav__logo-title">Vitterys</h1>
+            <h3 className="nav__logo-subtitle">Tu supermercado de confianza</h3>
+          </div>
+        </div>
       </Link>
     </div>
   );
