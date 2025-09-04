ae6b4407170194a9053a883a9e656307803474f4
Alexander Salvador <alexander_mya@outlook.com>
2025-09-03 22:35:54 -0500

    chore: optimizing header

--- src/Components/Header/NavMenu/NavMenu.jsx
+++ src/Components/Header/NavMenu/NavMenu.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
-import { IoIosArrowDown } from 'react-icons/io';
-import { IoIosMenu } from 'react-icons/io';
-
+import { useState, useRef } from 'react';
+import { IoIosArrowDown, IoIosMenu } from 'react-icons/io';
 import CategoryNavMenu from '../CategoryNavMenu/CategoryNavMenu';
 import './NavMenu.css';
 
 const NavMenu = () => {
   const [showCategories, setShowCategories] = useState(false);
+  const timeoutRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    setShowCategories(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setShowCategories(false);
+    }, 200); // <-- delay en milisegundos (200ms recomendado)
+  };
 
   return (
     <ul className="nav__menu-list">
       <li
         className="nav__menu-elements nav__menu-categorias"
-        onMouseEnter={() => setShowCategories(true)}
-        onMouseLeave={() => setShowCategories(false)}>
-        <span className="nav__menu-item">
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
+        <span className={`nav__menu-item ${showCategories ? 'active' : ''}`}>
           <IoIosMenu className="nav__menu-item-drowmenu" />
           <h2 className="nav__menu-item-text">Categorias</h2>
-          <IoIosArrowDown className="nav__menu-item-arrow-icon" />
+          <IoIosArrowDown
+            className={`nav__menu-item-arrow-icon ${
+              showCategories ? 'rotated' : ''
+            }`}
+          />
         </span>
 
         {showCategories && (
